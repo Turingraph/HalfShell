e088ae9674553c57f7f6ae633d14d2160aac49ee
Phuri Sottatipreedawong <phsottat@z2t17c3.42bangkok.com>
2026-09-15 13:30:43 +0700

    Make echo command

--- main.c
+++ main.c
@@ -1,16 +1,5 @@
 #include "include/halfshell.h"
 
-// volatile is used for making OS stop optimizing the program,
-// such that the program might works slower but more correctly.
-// sig_atomic_t is just the data type that you are allowed to use in the
-//  context of a signal handler. It is usually an int type.
-// the g_activation is used for making client wait before the server
-// finishing processing the individual bits, so both 2 programs are synchronized
-// and thus works more reliable.
-// https://stackoverflow.com/questions/24931456/
-// how-does-sig-atomic-t-actually-work
-volatile sig_atomic_t	g_activation = 1;
-
 // time : O(n)
 // space: O(1)
 size_t	f_strlen(const char *str)
@@ -28,47 +17,95 @@ size_t	f_strlen(const char *str)
 
 // time : O(n)
 // space: O(1)
-char	is_2_str_same(const char *a, const char *b)
+bool	is_2_str_same(const char *a, const char *b, size_t length)
 {
 	size_t	i;
 
 	if (a == NULL && b == NULL)
-		return (1);
+		return (true);
 	if (a == NULL || b == NULL)
-		return (0);
+		return (false);
 	i = 0;
-	while (a[i] == b[i] && a[i] != '\0')
+	while (a[i] != '\0' && i < length)
+	{
+		if (a[i] != b[i])
+			return (false);
 		i += 1;
-	if (a[i] != b[i])
-		return (0);
-	return (1);
+	}
+	return (true);
 }
 
-// https://medium.com/@levequeloris66/what-are-the-signals-in-c-3208d86c183e
-void	stop_func(volatile int signum)
+bool	is_contains_n_as(const char *src, char a, size_t n)
 {
-	char	*listenner;
+	size_t	i;
+	size_t	y;
 
-	listenner = readline("\nminishell $ ");
-	if (signum == SIGINT)
+	if (src == NULL || src[0] == '\0')
+		return (false);
+	y = 0;
+	i = 0;
+	while (src[i] != '\0')
 	{
-		if (is_2_str_same("exit", listenner) == true)
-			g_activation = 0;
-		free(listenner);
+		if (src[i] == a)
+			y += 1;
+		i += 1;
+	}
+	if (y != n)
+		return (false);
+	return (true);
+}
+
+void	command_echo(const char *str)
+{
+	size_t	i;
+
+	if (str == NULL)
+		return ;
+	i = 0;
+	while (str[i] == ' ')
+		i += 1;
+	if (is_contains_n_as((const char *)str, '"', 0) == true)
+	{
+		while (str[i] != '\0' && str[i] != ' ')
+		{
+			write(1, str + i, 1);
+			i += 1;
+		}
+	}
+	else if (str[i] == '"'
+		&& is_contains_n_as((const char *)str, '"', 2) == true)
+	{
+		i += 1;
+		while (str[i] != '\0' && str[i] != '"')
+		{
+			write(1, str + i, 1);
+			i += 1;
+		}
 	}
 }
 
-// ft_putnbr_fd write the PID of this server.
 int	main(void)
 {
-	struct sigaction	sa;
+	char	*listener;
+	int		g_activation;
 
-	sa.sa_handler = stop_func;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_SIGINFO;
-	sigaction(SIGINT, &sa, NULL);
+	g_activation = 1;
 	while (g_activation == 1)
-		pause();
+	{
+		listener = readline("minishell $ ");
+		if (listener == NULL)
+			break ;
+		else if (is_2_str_same("exit", listener, 4))
+			g_activation = 0;
+		else if (is_2_str_same("echo -n ", listener, 8))
+			command_echo(listener + 8);
+		else if (is_2_str_same("echo ", listener, 5))
+		{
+			command_echo(listener + 5);
+			write(1, "\n", 1);
+		}
+		free(listener);
+	}
 	return (0);
 }
 
